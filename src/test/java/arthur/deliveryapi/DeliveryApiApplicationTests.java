16eb090ee65c259c9072eb33b0d10d1d58a0ff3e
naocya@naver.com <naocya@naver.com>
2021-11-29 11:00:23 +0900

    first commit2

---
+++ src/test/java/arthur/deliveryapi/DeliveryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package arthur.deliveryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
