275e9109dd16e51e343e76bde2066e180abf1289
naocya@naver.com <naocya@naver.com>
2021-12-03 18:43:35 +0900

    done

--- src/main/java/arthur/deliveryapi/config/SwaggerConfiguration.java
+++ src/main/java/arthur/deliveryapi/config/SwaggerConfiguration.java
@@ -17,7 +17,10 @@ public class SwaggerConfiguration {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+//                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("arthur.deliveryapi"))
+                // 스웨거가 RestController를 전부 스캔을 한다.
+                // basePackage => 어디를 범위로 스캔을 할 것인지 작성
                 .paths(PathSelectors.any())
                 .build()
                 .apiInfo(apiInfo());
@@ -26,12 +29,14 @@ public class SwaggerConfiguration {
     private ApiInfo apiInfo() {
 
         return new ApiInfoBuilder()
-                .title("결제 API 서비스")
-                .description("결제 API 서비스 입니다")
-                .version("0.8.0")
-                .termsOfServiceUrl("http://www.XXX.com")
+                .title("음식 주문 서비스")
+                .description("음식점 등록 및 음식 주문을 할 수 있는 서비스입니다.")
+                .version("1.0.0")
+                .termsOfServiceUrl("https://antstudy.tistory.com/")
                 .license("LICENSE")
                 .licenseUrl("")
                 .build();
     }
+
+    // 완료가 되었으면 오른쪽 URL 로 접속 => http://localhost:8080/swagger-ui.html
 }
